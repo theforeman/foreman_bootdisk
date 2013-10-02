e7cac6c447122ab72c15bc695195f7692fbe1393
Dominic Cleal <dcleal@redhat.com>
2013-10-02 21:09:08 +0100

    Fix RHN_PROVISIONING_MAC_0 error on template retrieval when using static disk

--- app/controllers/concerns/bootdisk/unattended_controller_ext.rb
+++ app/controllers/concerns/bootdisk/unattended_controller_ext.rb
@@ -6,7 +6,7 @@ module Bootdisk::UnattendedControllerExt
   end
 
   def find_host_by_ip_or_mac_with_param_mac
-    request.env['HTTP_X_RHN_PROVISIONING_MAC_0'] = "unknown #{params['mac']}" unless request.env.has_key?('HTTP_X_RHN_PROVISIONING_MAC_0')
+    request.env['HTTP_X_RHN_PROVISIONING_MAC_0'] = "unknown #{params['mac']}" unless request.env.has_key?('HTTP_X_RHN_PROVISIONING_MAC_0') || params['mac'].nil?
     find_host_by_ip_or_mac_without_param_mac
   end
 end
