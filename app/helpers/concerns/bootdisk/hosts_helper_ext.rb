51bcb1b5b69efd9730cab0926bf5e782176840f7
Dominic Cleal <dcleal@redhat.com>
2014-01-21 22:49:19 +0000

    Fix host#show extension on Foreman 1.4

--- app/helpers/concerns/bootdisk/hosts_helper_ext.rb
+++ app/helpers/concerns/bootdisk/hosts_helper_ext.rb
@@ -8,7 +8,7 @@ module Bootdisk::HostsHelperExt
   def host_title_actions_with_bootdisk(host, vm)
     title_actions(
       button_group(
-        select_action_button(_('Boot disk'),
+        select_action_button(_('Boot disk'), {},
           display_link_if_authorized(_("Host '%s' image") % @host.name.split('.')[0], hash_for_bootdisk_iso_host_path(:id => @host), :class=>'btn'),
           display_link_if_authorized(_("Generic image"), hash_for_generic_iso_disks_path, :class=>'btn')
         )
