8fa4c43aeef950aac41f1a943a0ece83e0a285c1
Dominic Cleal <dcleal@redhat.com>
2014-11-17 14:15:28 +0000

    Fix seed idempotency test, only save when template changes made

--- db/seeds.d/50-bootdisk_templates.rb
+++ db/seeds.d/50-bootdisk_templates.rb
@@ -2,24 +2,32 @@ kind = TemplateKind.find_or_create_by_name('Bootdisk')
 
 ConfigTemplate.without_auditing do
   content = File.read(File.join(ForemanBootdisk::Engine.root, 'app', 'views', 'foreman_bootdisk', 'host.erb'))
-  ConfigTemplate.find_or_create_by_name(
+  tmpl = ConfigTemplate.find_or_create_by_name(
     :name => 'Boot disk iPXE - host',
     :template_kind_id => kind.id,
     :snippet => false,
     :template => content
-  ).update_attributes(:template => content,
-                      :default  => true,
-                      :vendor   => "Foreman boot disk",
-                      :locked   => true)
+  )
+  tmpl.attributes = {
+    :template => content,
+    :default  => true,
+    :vendor   => "Foreman boot disk",
+    :locked   => true
+  }
+  tmpl.save!(:validate => false) if tmpl.changes.present?
 
   content = File.read(File.join(ForemanBootdisk::Engine.root, 'app', 'views', 'foreman_bootdisk', 'generic_host.erb'))
-  ConfigTemplate.find_or_create_by_name(
+  tmpl = ConfigTemplate.find_or_create_by_name(
     :name => 'Boot disk iPXE - generic host',
     :template_kind_id => kind.id,
     :snippet => false,
     :template => content
-  ).update_attributes(:template => content,
-                      :default  => true,
-                      :vendor   => "Foreman boot disk",
-                      :locked   => true)
+  )
+  tmpl.attributes = {
+    :template => content,
+    :default  => true,
+    :vendor   => "Foreman boot disk",
+    :locked   => true
+  }
+  tmpl.save!(:validate => false) if tmpl.changes.present?
 end
