429251df1ee2056af215abdfd6e2fbfc7cacaf72
Dominic Cleal <dcleal@redhat.com>
2015-03-27 07:43:57 +0000

    Remove < 1.5 compatibility warning

--- app/services/foreman_bootdisk/renderer.rb
+++ app/services/foreman_bootdisk/renderer.rb
@@ -6,10 +6,6 @@ module ForemanBootdisk
     include Rails.application.routes.url_helpers
 
     def generic_template_render
-      if (Gem::Version.new(SETTINGS[:version].notag) < Gem::Version.new('1.5')) && Setting[:safemode_render]
-        raise(::Foreman::Exception.new(N_('Bootdisk is not supported with safemode rendering, please disable safemode_render under Adminster>Settings')))
-      end
-
       tmpl = ConfigTemplate.find_by_name(Setting[:bootdisk_generic_host_template]) || raise(::Foreman::Exception.new(N_('Unable to find template specified by %s setting'), 'bootdisk_generic_host_template'))
       @host = Struct.new(:token, :subnet).new(nil, nil)
       unattended_render(tmpl.template)
