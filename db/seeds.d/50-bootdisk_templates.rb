ab0c39daaa086d82d720de6525b8db173d787ee0
Marek Hulan <mhulan@redhat.com>
2016-04-18 12:44:41 +0200

    Fixes #14682 - associate orgs and locs to seeded templates

--- db/seeds.d/50-bootdisk_templates.rb
+++ db/seeds.d/50-bootdisk_templates.rb
@@ -1,7 +1,12 @@
 kind = TemplateKind.where(:name => 'Bootdisk').first_or_create
 
+organizations = Organization.all
+locations = Location.all
+created = []
+
 ProvisioningTemplate.without_auditing do
   content = File.read(File.join(ForemanBootdisk::Engine.root, 'app', 'views', 'foreman_bootdisk', 'host.erb'))
+  created << 'Boot disk iPXE - host' unless ProvisioningTemplate.find_by_name('Boot disk iPXE - host')
   tmpl = ProvisioningTemplate.where(:name => 'Boot disk iPXE - host').first_or_create do |template|
     template.attributes = {
       :template_kind_id => kind.id,
@@ -18,6 +23,7 @@ ProvisioningTemplate.without_auditing do
   tmpl.save!(:validate => false) if tmpl.changes.present?
 
   content = File.read(File.join(ForemanBootdisk::Engine.root, 'app', 'views', 'foreman_bootdisk', 'generic_host.erb'))
+  created << 'Boot disk iPXE - generic host' unless ProvisioningTemplate.find_by_name('Boot disk iPXE - generic host')
   tmpl = ProvisioningTemplate.where(:name => 'Boot disk iPXE - generic host').first_or_create do |template|
     template.attributes = {
       :template_kind_id => kind.id,
@@ -32,4 +38,9 @@ ProvisioningTemplate.without_auditing do
   }
   tmpl.locked = true
   tmpl.save!(:validate => false) if tmpl.changes.present?
+
+  ProvisioningTemplate.where(:name => created, :default => true).each do |tmpl|
+    tmpl.organizations = organizations if SETTINGS[:organizations_enabled]
+    tmpl.locations = locations if SETTINGS[:locations_enabled]
+  end
 end
