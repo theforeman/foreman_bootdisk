51a5589d6137f65f639e784f739ab71528203ccd
Dominic Cleal <dominic@cleal.org>
2016-05-11 08:06:35 +0100

    Change templates settings to return a lambda for live results

--- app/models/setting/bootdisk.rb
+++ app/models/setting/bootdisk.rb
@@ -5,13 +5,14 @@ class Setting::Bootdisk< ::Setting
 
     ipxe = ['/usr/lib/ipxe'].find { |p| File.exist?(p) } || '/usr/share/ipxe'
     syslinux = ['/usr/lib/syslinux'].find { |p| File.exist?(p) } || '/usr/share/syslinux'
+    templates = -> { Hash[ProvisioningTemplate.where(:template_kind => TemplateKind.where(:name => 'Bootdisk')).map{|temp| [temp[:name], temp[:name]]}] }
 
     Setting.transaction do
       [
         self.set('bootdisk_ipxe_dir', N_('Path to directory containing iPXE images'), ipxe),
         self.set('bootdisk_syslinux_dir', N_('Path to directory containing syslinux images'), syslinux),
-        self.set('bootdisk_host_template', N_('iPXE template to use for host-specific boot disks'), 'Boot disk iPXE - host', 'bootdisk_host_template', nil, { :collection => Hash[ProvisioningTemplate.where(:template_kind => TemplateKind.where(:name => 'Bootdisk')).map{|temp| [temp[:name], temp[:name]]}]}),
-        self.set('bootdisk_generic_host_template', N_('iPXE template to use for generic host boot disks'), 'Boot disk iPXE - generic host', 'bootdisk_generic_host_template', nil, { :collection => Hash[ProvisioningTemplate.where(:template_kind => TemplateKind.where(:name => 'Bootdisk')).map{|temp| [temp[:name], temp[:name]]}]}),
+        self.set('bootdisk_host_template', N_('iPXE template to use for host-specific boot disks'), 'Boot disk iPXE - host', 'bootdisk_host_template', nil, :collection => templates),
+        self.set('bootdisk_generic_host_template', N_('iPXE template to use for generic host boot disks'), 'Boot disk iPXE - generic host', 'bootdisk_generic_host_template', nil, :collection => templates),
         self.set('bootdisk_mkiso_command', N_('Command to generate ISO image, use genisoimage or mkisofs'), 'genisoimage'),
         self.set('bootdisk_cache_media', N_('Installation media files will be cached for full host images'), true),
       ].compact.each { |s| self.create s.update(:category => "Setting::Bootdisk") }
