ec0acf547388402fc4ac9f052453480b004cf88a
Ondrej Prazak <oprazak@redhat.com>
2017-01-20 14:02:31 +0100

    Fixes #17893 - Fix preview for bootdisk templates

--- app/services/foreman_bootdisk/renderer.rb
+++ app/services/foreman_bootdisk/renderer.rb
@@ -5,6 +5,7 @@ module ForemanBootdisk
     include ::Foreman::Renderer
     include Rails.application.routes.url_helpers
     delegate :logger, :to => :ForemanBootdisk
+    include RendererMethods
 
     def generic_template_render(subnet = nil)
       tmpl = ProvisioningTemplate.find_by_name(Setting[:bootdisk_generic_host_template]) || raise(::Foreman::Exception.new(N_('Unable to find template specified by %s setting'), 'bootdisk_generic_host_template'))
@@ -23,18 +24,6 @@ module ForemanBootdisk
       # remove the token from subnet-level generation, since this is meant to be generic
       modified_template.gsub!(/(?<=iPXE\?)token=faketoken\&(?=mac=)/,'')
       modified_template
-    end
-
-    def bootdisk_chain_url(mac = '', action = 'iPXE')
-      Renderer.format_bootdisk_chain_url(foreman_url(action), mac)
-    end
-
-    def self.format_bootdisk_chain_url(url, mac)
-      u = URI.parse(url)
-      ForemanBootdisk.logger.warn("Foreman or proxy is configured with HTTPS, probably not supported by iPXE: #{u}") if u.scheme == 'https'
-      u.query = "#{u.query}&mac=#{mac}"
-      u.fragment = nil
-      u.to_s
     end
   end
 end
---
+++ app/services/foreman_bootdisk/renderer_methods.rb
@@ -0,0 +1,19 @@
+module ForemanBootdisk
+  module RendererMethods
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def format_bootdisk_chain_url(url, mac)
+        u = URI.parse(url)
+        ForemanBootdisk.logger.warn("Foreman or proxy is configured with HTTPS, probably not supported by iPXE: #{u}") if u.scheme == 'https'
+        u.query = "#{u.query}&mac=#{mac}"
+        u.fragment = nil
+        u.to_s
+      end
+    end
+
+    def bootdisk_chain_url(mac = '', action = 'iPXE')
+      self.class.format_bootdisk_chain_url(foreman_url(action), mac)
+    end
+  end
+end
