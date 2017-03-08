9e3b8727728994decaf3f3a2baebb28d03c6a900
Lukas Zapletal <lzap+git@redhat.com>
2017-03-08 14:36:37 +0100

    Fixes #18823 - added tests for long filenames

--- test/unit/iso_generator_test.rb
+++ test/unit/iso_generator_test.rb
@@ -11,6 +11,34 @@ class ForemanBootdisk::IsoGeneratorTest < ActiveSupport::TestCase
     @host.build = true
   end
 
+  test "generate_full_host creates with ISO-compatible file names" do
+    @host.expects(:generate_pxe_template).with(:PXELinux).returns("Fake kernel line boot/Debian-8.1-x86_64-vmlinuz")
+    boot_files = [
+      {:"boot/Debian-8.1-x86_64"=>"/tmp/pxeboot/vmlinuz"},
+      {:"boot/Debian-8.1-x86_64"=>"/tmp/pxeboot/initrd.img"}
+    ]
+    @host.operatingsystem.expects(:pxe_files).returns(boot_files)
+    ForemanBootdisk::ISOGenerator.expects(:generate).with({
+      :isolinux => 'Fake kernel line BOOT/DEBIAN_8_1_X86_64_VMLINUZ',
+      :files => [[['BOOT/DEBIAN_8_1_X86_64_VMLINUZ', '/tmp/pxeboot/vmlinuz']],
+      [['BOOT/DEBIAN_8_1_X86_64_INITRD_IMG', '/tmp/pxeboot/initrd.img']]] }, anything)
+    ForemanBootdisk::ISOGenerator.generate_full_host(@host)
+  end
+
+  test "generate_full_host creates with ISO-compatible long file names" do
+    @host.expects(:generate_pxe_template).with(:PXELinux).returns("Fake kernel line boot/RedHatEnterpriseLinuxAtomic-7.3-x86_64-vmlinuz")
+    boot_files = [
+      {:"boot/RedHatEnterpriseLinuxAtomic-7.3-x86_64"=>"/tmp/pxeboot/vmlinuz"},
+      {:"boot/RedHatEnterpriseLinuxAtomic-7.3-x86_64"=>"/tmp/pxeboot/initrd.img"}
+    ]
+    @host.operatingsystem.expects(:pxe_files).returns(boot_files)
+    ForemanBootdisk::ISOGenerator.expects(:generate).with({
+      :isolinux => 'Fake kernel line BOOT/NUXATOMIC_7_3_X86_64_VMLINUZ',
+      :files => [[['BOOT/NUXATOMIC_7_3_X86_64_VMLINUZ', '/tmp/pxeboot/vmlinuz']],
+        [['BOOT/ATOMIC_7_3_X86_64_INITRD_IMG', '/tmp/pxeboot/initrd.img']]]}, anything)
+    ForemanBootdisk::ISOGenerator.generate_full_host(@host)
+  end
+
   test "full host image generation generates via PXELinux type" do
     @host.operatingsystem.expects(:pxe_files).returns([])
     @host.operatingsystem.expects(:kernel).returns("kernel")
