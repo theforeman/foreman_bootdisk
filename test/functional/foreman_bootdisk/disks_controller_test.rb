a5e3435d4c9b905dbf5464432e55011ae6778690
Michael Moll <kvedulv@kvedulv.de>
2017-12-28 19:46:17 +0100

    stop using HttpPositionalArguments

--- test/functional/foreman_bootdisk/disks_controller_test.rb
+++ test/functional/foreman_bootdisk/disks_controller_test.rb
@@ -7,7 +7,7 @@ class ForemanBootdisk::DisksControllerTest < ActionController::TestCase
   test "should generate generic image" do
     ForemanBootdisk::ISOGenerator.expects(:generate).with(has_entry(:ipxe => regexp_matches(/disk generic host template/))).yields("temp ISO")
     @controller.expects(:read_file).with("temp ISO").returns("ISO image")
-    get :generic, {}, set_session_user
+    get :generic, session: set_session_user
     assert_response :success
     assert_equal "ISO image", @response.body
   end
@@ -21,7 +21,7 @@ class ForemanBootdisk::DisksControllerTest < ActionController::TestCase
     test "should generate host image" do
       ForemanBootdisk::ISOGenerator.expects(:generate).with(has_entry(:ipxe => regexp_matches(/disk host template/))).yields("temp ISO")
       @controller.expects(:read_file).with("temp ISO").returns("ISO image")
-      get :host, {:id => @host.name}, set_session_user
+      get :host, params: {:id => @host.name}, session: set_session_user
       assert_response :success
       assert_equal "ISO image", @response.body
     end
@@ -29,7 +29,7 @@ class ForemanBootdisk::DisksControllerTest < ActionController::TestCase
     test "should generate full host image" do
       ForemanBootdisk::ISOGenerator.expects(:generate_full_host).with(@host).yields("temp ISO")
       @controller.expects(:read_file).with("temp ISO").returns("ISO image")
-      get :full_host, {:id => @host.name}, set_session_user
+      get :full_host, params: {:id => @host.name}, session: set_session_user
       assert_response :success
       assert_equal "ISO image", @response.body
     end
@@ -38,7 +38,7 @@ class ForemanBootdisk::DisksControllerTest < ActionController::TestCase
       ForemanBootdisk::ISOGenerator.expects(:generate).with(has_entry(:ipxe => regexp_matches(/disk generic host template/))).yields("temp ISO")
       ForemanBootdisk::Renderer.any_instance.stubs(:bootdisk_chain_url).yields("http://smart-proxy.lan")
       @controller.expects(:read_file).with("temp ISO").returns("ISO image")
-      get :subnet, {:id => @host.name}, set_session_user
+      get :subnet, params: {:id => @host.name}, session: set_session_user
       assert_empty flash[:error]
       assert_response :success
       assert_equal "ISO image", @response.body
@@ -52,14 +52,14 @@ class ForemanBootdisk::DisksControllerTest < ActionController::TestCase
     setup :setup_host
 
     test "should not generate subnet image" do
-      get :subnet, {:id => @host.name}, set_session_user
+      get :subnet, params: {:id => @host.name}, session: set_session_user
       assert_match(/Failed.*: TFTP feature not enabled/, flash[:error])
       assert_response :redirect
     end
   end
 
   test "should render help" do
-    get :help, {}, set_session_user
+    get :help, session: set_session_user
     assert_response :success
   end
 end
