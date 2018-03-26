385199f745c6643210b3c2d5ccd5d53fb08f0225
Michael Hofer <karras@users.noreply.github.com>
2018-03-26 13:17:20 +0200

    Fixes #22858 - Move MAC based identification to Foreman core (#58)

--- app/controllers/concerns/foreman_bootdisk/unattended_controller_ext.rb
+++
@@ -1,6 +0,0 @@
-module ForemanBootdisk::UnattendedControllerExt
-  def find_host_by_ip_or_mac
-    request.env['HTTP_X_RHN_PROVISIONING_MAC_0'] = "unknown #{params['mac']}" unless request.env.has_key?('HTTP_X_RHN_PROVISIONING_MAC_0') || params['mac'].nil?
-    super
-  end
-end
