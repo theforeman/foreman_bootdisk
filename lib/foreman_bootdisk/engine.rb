46df9e52d89df9876759dc690e1d8a6243938fcc
Lukas Zapletal <lzap+git@redhat.com>
2022-03-24 15:44:55 +0100

    Fixes #34674 - deprecate non-DSL settings
    Signed-off-by: Lukas Zapletal <lzap+git@redhat.com>

--- lib/foreman_bootdisk/engine.rb
+++ lib/foreman_bootdisk/engine.rb
@@ -17,15 +17,6 @@ module ForemanBootdisk
       app.routes_reloader.paths << "#{ForemanBootdisk::Engine.root}/config/routes/mount_engine.rb"
     end
 
-    initializer 'foreman_bootdisk.load_default_settings', before: :load_config_initializers do |_app|
-      table_exists = begin
-                       Setting.table_exists?
-                     rescue StandardError
-                       false
-                     end
-      require_dependency File.expand_path('../../app/models/setting/bootdisk.rb', __dir__) if table_exists
-    end
-
     initializer 'foreman_bootdisk.load_app_instance_data' do |app|
       ForemanBootdisk::Engine.paths['db/migrate'].existent.each do |path|
         app.config.paths['db/migrate'] << path
@@ -50,7 +41,6 @@ module ForemanBootdisk
         end
 
         role 'Boot disk access', [:download_bootdisk], 'Role granting permissions to download bootdisks'
-
         add_all_permissions_to_default_roles
 
         apipie_documented_controllers ["#{ForemanBootdisk::Engine.root}/app/controllers/foreman_bootdisk/api/v2/*.rb"]
@@ -64,6 +54,81 @@ module ForemanBootdisk
         extend_page "subnets/index" do |cx|
           cx.add_pagelet :subnet_index_action_buttons, name: 'Bootdisk', partial: 'subnets/bootdisk_action_buttons'
           cx.add_pagelet :subnet_index_title_buttons, name: 'Bootdisk', partial: 'subnets/bootdisk_title_buttons'
+        end
+
+        settings do
+          category :bootdisk, N_("Boot disk") do
+            ipxe = ['/usr/lib/ipxe'].find { |p| File.exist?(p) } || '/usr/share/ipxe'
+            isolinux = ['/usr/lib/ISOLINUX'].find { |p| File.exist?(p) } || '/usr/share/syslinux'
+            syslinux = ['/usr/lib/syslinux/modules/bios', '/usr/lib/syslinux'].find { |p| File.exist?(p) } || '/usr/share/syslinux'
+            grub2 = ['/var/lib/tftpboot/grub2'].find { |p| File.exist?(p) } || '/var/lib/foreman/bootdisk'
+            templates = -> {
+              Hash[ProvisioningTemplate.where(template_kind: TemplateKind.where(name: 'Bootdisk')).map { |temp| [temp[:name], temp[:name]] }]
+            }
+
+            setting "bootdisk_ipxe_dir",
+              type: :string,
+              default: ipxe,
+              full_name: N_("iPXE directory"),
+              description: N_("Path to directory containing iPXE images")
+
+            setting "bootdisk_isolinux_dir",
+              type: :string,
+              default: isolinux,
+              full_name: N_("ISOLINUX directory"),
+              description: N_("Path to directory containing isolinux images")
+
+            setting "bootdisk_syslinux_dir",
+              type: :string,
+              default: syslinux,
+              full_name: N_("SYSLINUX directory"),
+              description: N_("Path to directory containing syslinux images")
+
+            setting "bootdisk_grub2_dir",
+              type: :string,
+              default: grub2,
+              full_name: N_("Grub2 directory"),
+              description: N_("Path to directory containing grubx64.efi and shimx64.efi")
+
+            setting "bootdisk_host_template",
+              type: :string,
+              collection: templates,
+              default: "Boot disk iPXE - host",
+              full_name: N_("Host image template"),
+              description: N_("iPXE template to use for host-specific boot disks")
+
+            setting "bootdisk_generic_host_template",
+              type: :string,
+              collection: templates,
+              default: "Boot disk iPXE - generic host",
+              full_name: N_("Generic image template"),
+              description: N_("iPXE template to use for generic host boot disks")
+
+            setting "bootdisk_generic_efi_host_template",
+              type: :string,
+              collection: templates,
+              default: "Boot disk Grub2 EFI - generic host",
+              full_name: N_("Generic Grub2 EFI image template"),
+              description: N_("iPXE template to use for generic EFI host boot disks")
+
+            setting "bootdisk_mkiso_command",
+              type: :string,
+              default: "genisoimage",
+              full_name: N_("ISO generation command"),
+              description: N_("Command to generate ISO image, use genisoimage or mkisofs")
+
+            setting "bootdisk_cache_media",
+              type: :boolean,
+              default: true,
+              full_name: N_("Installation media caching"),
+              description: N_("Installation media files will be cached for full host images")
+
+            setting "bootdisk_allowed_types",
+              type: :string,
+              default: Setting::Bootdisk.bootdisk_types,
+              full_name: N_("Allowed bootdisk types"),
+              description: N_("List of allowed bootdisk types, remove type to disable it")
+          end
         end
       end
     end
